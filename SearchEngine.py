c0c63683a0277fa3a38bf5546455149670bc8278
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2022-05-23 16:57:48 +0700

    Add files via upload
    Fixing Bugs, algorithm

---
+++ SearchEngine.py
@@ -0,0 +1,44 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import string
+import numpy as np
+from TrueSightEngine import SearchEngine as TS
+from TrueSightEngine import TimeExecution
+from datetime import datetime
+
+
+# buildder = {}
+
+# TS.addDataToDictionary({'berita': 'berita ke1', 'isi': 'random1'}, buildder)
+# TS.addDataToDictionary({'berita': 'berita ke2', 'isi': 'random2'}, buildder)
+# TS.addDataToDictionary({'berita': 'berita ke3', 'isi': 'random3'}, buildder)
+
+# print(buildder)
+
+file_csv = input("Lokasi csv: ")
+reader = pd.read_csv(file_csv)
+
+header = input("Lihat Kolom Header: ")
+cari = input('Cari kalimat: ')
+
+data = reader.to_dict()
+
+TE = TimeExecution()
+TE.init()
+
+result = TS.search_from_dict(
+    keywords=cari,
+    data=data,
+    lookupHeader=[header]
+)
+print("Total time spend")
+TE.end()
+
+THRESHOLD = 0.1
+
+for i, (v, x) in enumerate(result):
+    if v > THRESHOLD:
+        print(str(i) + f"({str(v)})", '=>', x['berita'])
+
+if len(result) == 0:
+    print("Not found")
